5f89be2cc5ead9b9b59e94bfb3ddfaea6a144e73
Steve2404 <jatsasteve@gmail.com>
2024-08-14 18:23:21 +0200

    debut vue fonde sur les classes

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.views.generic import View
 
+from authentication import forms
 from authentication.forms import LoginForm
 
 
@@ -27,3 +29,28 @@ def login_page(request):
             else:
                 message = "Identifiants Invalides ..."
     return render(request, 'authentication/login.html', context={'form': form, 'message': message})
+
+
+class LoginPageView(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name,
+                      context={'form': form, 'message': message})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            message = "Identifiants Invalides ..."
+            return render(request, self.template_name,
+                          context={'form': form, 'message': message})
